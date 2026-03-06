c247dae6c3699e8e4a5e0099cdc5ce5d29696978
Nicole Monteiro Torres <duq0104759@aluno.firjansenaisesi.com.br>
2026-03-06 16:13:06 -0300

    alterações 06/03

---
+++ react-vite/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+function Home(){
+    return(
+        <>
+           <h1>Home</h1>
+           
+           <h3>Seja Bem-vindo(a)</h3>
+           <p>
+           A TechSolutions desenvolve produtos tecnológicos voltados para facilitar o dia a dia de empresas e profissionais. <br />
+           Seus produtos incluem sistemas de gestão, plataformas digitais e aplicações desenvolvidas para melhorar a organização, <br />
+           a produtividade e a comunicação dentro das empresas. Todos os produtos são criados com foco em inovação, <br />
+           segurança e facilidade de uso, ajudando os clientes a acompanhar as mudanças do mercado e a otimizar seus processos de trabalho.
+           </p>
+        </>
+     
+        
+
+    )
+}
+export default Home
